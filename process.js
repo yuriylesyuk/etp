58b5774e828f72808861b9fa2fbc480c0a274cd5
Yuriy Lesyuk <ylesyuk@apigee.com>
2017-04-21 17:22:55 +0100

    ansible checkports.yml compatible input file.

--- process.js
+++ process.js
@@ -4,7 +4,7 @@ var fp = require("lodash/fp");
 
 var portDefs = require("./edge-port-defs.json");
 
-//var topology = require("./enbd-uat.topology.json");
+//var topology = require("./uat-12n-4sn-topology.json");
 var topology = require("./uat-19n-3sn-topology.json");
 
 
@@ -49,76 +49,6 @@ var edge_mon = [ "TG", "IF", "GF" ];
 
 
 
-
-
-console.log(fp(topology.layout[0].components).includes("MP"));
-
-var clientnodes = fp.filter( n => fp(n.components).includes("R") )(topology.layout).map(n=>n.node);
-
-var servernodes_r = fp.filter( n => fp(n.components).includes("R") )(topology.layout).map(n=>n.node);
-
-var servernodes = fp.filter( n => fp(n.components).includes("MP") )(topology.layout).map(n=>n.node);
-console.log(servernodes);
-
-var subnet = fp.filter( s => fp(s.nodes).includes(9))(topology.subnets).map(s=>s.name) ;
-
-var clientsubnets = fp.flatMap( n => fp.filter( s => fp(s.nodes).includes(n))(topology.subnets).map(s=>{ return { node: n, subnet: s.name }; }) )(clientnodes);
-
-var serversubnets_r = fp.flatMap( n => fp.filter( s => fp(s.nodes).includes(n))(topology.subnets).map(s=>{ return { node: n, subnet: s.name }; }) )(servernodes_r);
-var serversubnets = fp.flatMap( n => fp.filter( s => fp(s.nodes).includes(n))(topology.subnets).map(s=>{ return { node: n, subnet: s.name }; }) )(servernodes);
-
-console.log(clientsubnets);
-console.log(serversubnets);
-// R=>R
-var n_r = fp.flatMap( cs => 
-     fp.reduce( (plist, ss ) => {
-            ss.subnet == cs.subnet ? plist : plist.push( { src: cs.node, dst: ss.node, port: fp.find({'component':"R"})(portDefs.edge.client) } );
-            return plist;
-         },[])(serversubnets_r) 
-   )(clientsubnets)
-console.log("--------")
-console.log(n_r);
-// R=>MP
-var n = fp.flatMap( cs => 
-     fp.reduce( (plist, ss ) => {
-            ss.subnet == cs.subnet ? plist : plist.push( { src: cs.node, dst: ss.node, port: "<ports>"} );
-            return plist;
-         },[])(serversubnets) 
-   )(clientsubnets)
-
-console.log(n);
-//var fwrequest = fp.reduce( (l, i)=> {
-//    i.
-//    return l;
-//}, [] )
-
-
-
-
-//console.log( fp.filter(topology.layout, i => fp.includes(i.components, "MP");
-//fp.includes(topology.layout)
-// nested lookup:
-//var cc = fp.filter({client: [{component: "R"}]})(portDefs.edge);
-//var cc = fp.filter( ['client.component',"R" ] )(portDefs.edge).filter(['servers.component', "MP"]);
-
-var cc = fp.filter(['client.component', "R"])(portDefs.edge)[0].client;
-var ss = fp.filter(['component', "MP"])(cc.servers)[0];
-
-
-
-console.log(ss.ports);
-
-// same but one-liner
-
-// TODO: later or maybe
-var ports = fp.filter(['component', "MP"])((fp.filter(['client.component', "R"])(portDefs.edge))[0].client.servers);
-
-
-var ports = fp.fi
-console.log( ports);
-
-console.log("====================================================");
-
 var nodes = fp.keyBy( "id" )( topology.nodes );
 
 
@@ -137,7 +67,7 @@ var firewallPortRequest = fp.map( clientComponent =>{
         var servernodes = fp.filter( n => fp(n.components).includes( sc.component ) )(topology.layout).map(n=>n.node);
         var serversubnets = fp.flatMap( n => fp.filter( s => fp(s.nodes).includes(n))(topology.subnets).map(s=>{ return { node: n, subnet: s.name }; }) )(servernodes);
         
-        console.log(servernodes)
+        // console.log(servernodes)
 
 
         // bottom-level reduce by subnet analysis
@@ -146,7 +76,7 @@ var firewallPortRequest = fp.map( clientComponent =>{
                     ss.subnet == cs.subnet ? plist : plist.push( 
                         // flatMap by sc.ports
                         // { src: cs.node, dst: ss.node, port: sc.ports }
-                        fp.map( port => { return { srcnode: cs.node, srchostname: nodes[cs.node].hostname,  dstnode: ss.node, dsthostname: nodes[ss.node].hostname, clientcomponent: clientComponent, servercomponent: sc.component, port: port }; } )(sc.ports)
+                        fp.map( port => { return { srcnode: cs.node, srchostname: nodes[cs.node].hostname,  srcip: nodes[cs.node].ip, dstnode: ss.node, dsthostname: nodes[ss.node].hostname, dstip: nodes[ss.node].ip, clientcomponent: clientComponent, servercomponent: sc.component, port: port }; } )(sc.ports)
                     );
                     return plist;
                 }, firewallPortRequests )(serversubnets) 
@@ -156,13 +86,38 @@ var firewallPortRequest = fp.map( clientComponent =>{
     
 })(edge_int);
 
+var firewallPortRequestsList = fp.flattenDeep(firewallPortRequests);
+
+
+
+var testportcolumns = [ "c", "s", "p"]
+
+var testports = fp.flow(
+                fp.map( port => { return { c: port.srcip, s: port.dstip, p: port.port }; } )
+    )(firewallPortRequestsList).reduce( (arr,elem) => { arr.push( elem ); return arr;}, [] ) ;
+
+// Generate test port input
+var testport = { ports: 
+                    testports
+                };
+
+fs = require('fs');
+fs.writeFile('firewallportstest.json', JSON.stringify(testport, null, 4), function (err) {
+    if (err) 
+        return console.log(err);
+    console.log("File successfully written out");
+});
+
+
+
+
+
 
 // Generate CSV 
 //console.log(firewallPortRequests);
 
 var columns = [ "srcnode", "srchostname", "dstnode", "dsthostname", "clientcomponent", "servercomponent", "port" ];
 
-var firewallPortRequestsList = fp.flattenDeep(firewallPortRequests);
 
 var csv = [ fp.chain( firewallPortRequestsList[0] ).pick(columns).keys().value().join( ", ") ];
 
