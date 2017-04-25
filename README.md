85a83deb71ea13a775ede38bb11d815c302c1f72
Yuriy Lesyuk <ylesyuk@apigee.com>
2017-04-25 20:21:35 +0100

    Refactor index.js

--- README.md
+++ README.md
@@ -16,4 +16,14 @@ Examples of configurations can be seen in examples foler.
 npm install -g https://github.com/yuriylesyuk/etp/tarball/master
 
 
+2. Fetch an example file to your working folder
 
+wget https://raw.githubusercontent.com/yuriylesyuk/etp/master/examples/uat-12n-4sn-topology.json
+
+
+etp generate portrequest ./uat-12n-4sn-topology.json ./portrequest.csv
+
+
+1. 
+
+etp generate diagram ./examples/uat-12n-4sn-topology.json svgdiagram.svg
--- index.js
+++ index.js
@@ -8,22 +8,22 @@ program
 program
     .command("generate <artifact> <topology> [<output>]").alias("g")
     .description('Generate artifact')
-    .option("-i")
-    .action(function(artifact, topology, output){
+    .action(function (artifact, topology, output) {
         artifactType = artifact || "<notprovided>";
         topologyFile = topology;
         outputFile = output || 'con';
     });
-  program.on('*', function () {
-      console.log('Unknown Command: ' + program.args.join(' '));
-      program.help();
-    })
 
-var supportedArtifactTypes = [ "diagram", "portrequest", "checkports" ];
+program.on('*', function () {
+    console.log('Unknown Command: ' + program.args.join(' '));
+    program.help();
+})
 
-program.on('--help', function(){
-  console.log(' Supported artifact:  ' + supportedArtifactTypes.join(", "));
-  console.log('');
+var supportedArtifactTypes = ["diagram", "portrequest", "checkports"];
+
+program.on('--help', function () {
+    console.log(' Supported artifact:  ' + supportedArtifactTypes.join(", "));
+    console.log('');
 });
 program.parse(process.argv);
 
@@ -32,12 +32,21 @@ if (!process.argv.slice(2).length) {
     process.exit(1);
 }
 
-if( supportedArtifactTypes.indexOf( artifactType ) == -1 ){
-   console.error(`The artifact '${artifactType}' is not supported.`);
-   process.exit(1);
+if (supportedArtifactTypes.indexOf(artifactType) == -1) {
+    console.error(`The artifact '${artifactType}' is not supported.`);
+    process.exit(1);
 }
 
-console.log(artifactType);
+//console.log(artifactType);
+//console.log(topologyFile);
+//console.log(outputFile);
 
-console.log(topologyFile);
-console.log(outputFile);
+if( artifactType === "portrequest"){
+    var generatePortRequest = require( "./generatePortRequest.js" );
+
+    generatePortRequest(topologyFile, outputFile);
+}else if (artifactType === "diagram" ){
+    var generateSvgDiagram = require( "./generateSvgDiagram.js" );
+
+    generateSvgDiagram(topologyFile, outputFile);
+}
