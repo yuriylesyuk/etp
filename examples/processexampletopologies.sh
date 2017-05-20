17e01140395f39284469bb5d8dc1f9a1553f3cfc
Yuriy Lesyuk <ylesyuk@apigee.com>
2017-05-20 12:13:06 +0100

    Fix -diagram extention typo

--- examples/processexampletopologies.sh
+++ examples/processexampletopologies.sh
@@ -2,14 +2,14 @@
 
 # regen all example topologies using dev-time etc execution
 
-node ../index.js g diagram $PWD/prod-2dc-19n-5sn-topology.json $PWD/prod-2dc-19n-5sn-diagram.json
+node ../index.js g diagram $PWD/prod-2dc-19n-5sn-topology.json $PWD/prod-2dc-19n-5sn-diagram.svg
 node ../index.js g portrequest $PWD/prod-2dc-19n-5sn-topology.json $PWD/prod-2dc-19n-5sn-portrequest.csv
 
-node ../index.js g diagram $PWD/uat-12n-4sn-topology.json $PWD/uat-12n-4sn-diagram.json
+node ../index.js g diagram $PWD/uat-12n-4sn-topology.json $PWD/uat-12n-4sn-diagram.svg
 node ../index.js g portrequest $PWD/uat-12n-4sn-topology.json $PWD/uat-12n-4sn-portrequest.csv
 
-node ../index.js g diagram $PWD/prod-1dc-19n-5sn-topology.json $PWD/prod-1dc-19n-5sn-diagram.json
+node ../index.js g diagram $PWD/prod-1dc-19n-5sn-topology.json $PWD/prod-1dc-19n-5sn-diagram.svg
 node ../index.js g portrequest $PWD/prod-1dc-19n-5sn-topology.json $PWD/prod-1dc-19n-5sn-portrequest.csv
 
-node ../index.js g diagram $PWD/uat-12n-4sn-allcore-v2-topology.json $PWD/uat-12n-4sn-allcore-v2-diagram.json
+node ../index.js g diagram $PWD/uat-12n-4sn-allcore-v2-topology.json $PWD/uat-12n-4sn-allcore-v2-diagram.svg
 node ../index.js g portrequest $PWD/uat-12n-4sn-allcore-v2-topology.json $PWD/uat-12n-4sn-allcore-v2-portrequest.csv
